bb79366a68e0e7bf808cb9d370f97d2821692214
takeshi8989 <htakeshi0614@gmail.com>
2024-02-26 11:48:04 -0800

    Delete unused article and update article details

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,39 +2,19 @@ export interface Article {
   id: string;
   en: boolean;
   ja: boolean;
-  author?: string;
+  author: string;
   created_at: string;
   updated_at?: string;
   title: Record<Language, string>;
   description: Record<Language, string>;
-  read_time_min?: number;
-  summary?: Record<Language, string>;
+  read_time_min: number;
+  summary: Record<Language, string>;
+  type: "Essay" | "Blog" | "Book" | "Movie" | "Music" | "Show";
 }
 
 export type Language = "en" | "ja";
 
 const data: Article[] = [
-  {
-    id: "new-year-resolution-2024",
-    en: false,
-    ja: false,
-    author: "Takeshi Hashimoto",
-    created_at: "2024-01-02",
-    updated_at: "2024-01-02",
-    title: {
-      en: "My New Year Resolution 2024",
-      ja: "2024年の抱負「殻を破って成果を出す」",
-    },
-    description: {
-      en: "I will start a new project in 2024.",
-      ja: "2023年の軽いまとめと2024年の抱負と目標",
-    },
-    read_time_min: 5,
-    summary: {
-      en: "I will start a new project in 2024.",
-      ja: "新しいことをどんどんやる。ビジネス、英語、対人能力、人間力、あらゆるモノを継続的に成長させる。そしてビジネスにおいて目にみえる成果を出す。やるぞ！",
-    },
-  },
   {
     id: "planned-happenstance",
     en: false,
@@ -50,11 +30,34 @@ const data: Article[] = [
       en: "Utilize random events to create new ideas and opportunities.",
       ja: "偶然の出来事を計画しておく。",
     },
-    // read_time_min: 5,
-    // summary: {
-    //   en: "",
-    //   ja: "",
-    // },
+    read_time_min: 5,
+    summary: {
+      en: "",
+      ja: "",
+    },
+    type: "Essay",
+  },
+  {
+    id: "about-english",
+    en: false,
+    ja: true,
+    author: "Takeshi Hashimoto",
+    created_at: "2024-02-26",
+    updated_at: "2024-02-26",
+    title: {
+      en: "About English",
+      ja: "英語について",
+    },
+    description: {
+      en: "English and my thoughts.",
+      ja: "自身の経験を踏まえた英語についての考察",
+    },
+    read_time_min: 5,
+    summary: {
+      en: "",
+      ja: "",
+    },
+    type: "Essay",
   },
 ];
 
