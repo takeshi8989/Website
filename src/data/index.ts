a0426311077764ef7f034a016ac1b40c508e2cd4
takeshi8989 <htakeshi0614@gmail.com>
2024-02-26 01:01:06 -0800

    Add new article about planned happenstance and update page component for English language

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,19 +2,16 @@ export interface Article {
   id: string;
   en: boolean;
   ja: boolean;
-  author: string;
+  author?: string;
   created_at: string;
-  updated_at: string;
+  updated_at?: string;
   title: Record<Language, string>;
   description: Record<Language, string>;
-  tags: Tag[];
-  read_time_min: number;
-  summary: Record<Language, string>;
+  read_time_min?: number;
+  summary?: Record<Language, string>;
 }
 
 export type Language = "en" | "ja";
-
-type Tag = "New Year Resolution";
 
 const data: Article[] = [
   {
@@ -32,13 +29,33 @@ const data: Article[] = [
       en: "I will start a new project in 2024.",
       ja: "2023年の軽いまとめと2024年の抱負と目標",
     },
-    tags: ["New Year Resolution"],
     read_time_min: 5,
     summary: {
       en: "I will start a new project in 2024.",
       ja: "新しいことをどんどんやる。ビジネス、英語、対人能力、人間力、あらゆるモノを継続的に成長させる。そしてビジネスにおいて目にみえる成果を出す。やるぞ！",
     },
-  }
+  },
+  {
+    id: "planned-happenstance",
+    en: false,
+    ja: true,
+    author: "Takeshi Hashimoto",
+    created_at: "2024-02-25",
+    updated_at: "2024-02-25",
+    title: {
+      en: "Planned Happenstance",
+      ja: "計画的偶発性",
+    },
+    description: {
+      en: "Utilize random events to create new ideas and opportunities.",
+      ja: "偶然の出来事を計画しておく。",
+    },
+    // read_time_min: 5,
+    // summary: {
+    //   en: "",
+    //   ja: "",
+    // },
+  },
 ];
 
 export const getArticlesByLanguage = (lang: Language): Article[] => {
