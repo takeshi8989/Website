80dc9cbc10a2f1f0275daa9d56aa8876bd1cb27c
takeshi8989 <htakeshi0614@gmail.com>
2025-01-05 17:10:36 -0800

    display articles

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,22 @@
-import data from "../../data/index.json"
+import { Link } from "react-router-dom";
+import data from "../../data/index.json";
+
+type Data = {
+  title: string;
+  body: string;
+};
 
 const Home = () => {
-  console.log(data);
-
-  return <div>Home, updated</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      {data.map((item: Data) => (
+        <div key={item.title}>
+          <Link to={`/ja/${item.title}`}>{item.title}</Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
